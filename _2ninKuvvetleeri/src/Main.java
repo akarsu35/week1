379e7ff878fb95915e9d927ec167e6a6ef4da0be
akarsu35 <cumhurakarsu@gmail.com>
2023-12-13 15:57:28 +0300

    second commit

--- _2ninKuvvetleeri/src/Main.java
+++ _2ninKuvvetleeri/src/Main.java
@@ -1,4 +1,3 @@
-
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
@@ -6,13 +5,16 @@ public class Main {
 
         Scanner input=new Scanner(System.in);
 
+        //kullanıcıdan veri alıyoruz.
         System.out.println("sınır sayıyı giriniz:");
         sayi=input.nextInt();
 
+        //kullanıcı taradından girilen sayıya kadar 4'ün kuvveti olan sayılar
         for(i=1;i<=sayi;i*=4){
             System.out.println("4'ün kuvvetleri olan sayılar: "+i);
 
         }
+        //kullanıcı taradından girilen sayıya kadar 5'in kuvveti olan sayılar
         for (j=1;j<=sayi;j*=5){
             System.out.println("5'in kuvvetleri olan sayılar: "+j);
         }
