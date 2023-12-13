25d951319118838e094ef007046a2a231910f014
akarsu35 <cumhurakarsu@gmail.com>
2023-12-13 16:12:51 +0300

    first commit

---
+++ faktoriyelHesaplama/src/Main.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        //c(n,r) = n! / (r! * (n-r)!)
+        int n,r,c,nfak=1,rfak=1,nr=1, cnr;
+
+        Scanner input=new Scanner(System.in);
+        System.out.println("kombinasyon değerini bulmak istediğiniz sayıyı giriniz: ");
+        n= input.nextInt();
+
+        System.out.println(n+" sayısının kaçlı kombinasyonunu bulmak istiyorsunuz: ");
+        r= input.nextInt();
+
+        while(r>n){
+            System.out.println(n+" sayısına eşit yada küçük bir sayı girmelisiniz.");
+            r=input.nextInt();
+        }
+        // n! hesaplama
+        for(int i=1;i <= n;i++){
+            nfak=nfak*i;
+            //System.out.println(nfak);
+        }
+
+        // r! hesaplama
+        for (int j=1;j <= r;j++){
+            rfak=rfak*j;
+            //System.out.println(rfak);
+        }
+
+        // (n-r)! hesaplama
+        for (int k=1;k <= (n-r);k++){
+            nr=nr*k;
+            //System.out.println(nr);
+        }
+
+
+        //c(n,r) hesaplama
+        cnr=nfak/(nr*rfak);
+        System.out.println("C("+n+", "+r+")= "+cnr);
+    }
+}
